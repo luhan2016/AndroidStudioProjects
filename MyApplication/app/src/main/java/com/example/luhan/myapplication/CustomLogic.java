e444edab6c75066278f8c38b73d1e0ba89fab46c
luhan2016 <luhansophia@gmail.com>
2018-02-21 10:29:25 +0100

    study android
    study android

---
+++ MyApplication/app/src/main/java/com/example/luhan/myapplication/CustomLogic.java
@@ -0,0 +1,11 @@
+package com.example.luhan.myapplication;
+
+/**
+ * Created by lu.han on 2018-02-21.
+ */
+
+public class CustomLogic {
+
+    // TODO FINISH ME
+
+}
--- MyApplication/app/src/main/java/com/example/luhan/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/luhan/myapplication/MainActivity.java
@@ -4,30 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setupUI();
-    }
-
-    private void setupUI(){
         setContentView(R.layout.activity_main);
-        mTextView = (TextView)findViewById(R.id.textView);
+
+        exampleMethod();
+        //TODO FINISH ME 2
     }
 
-    private final static String LOG_TAG = MainActivity.class.getSimpleName();
+    private void exampleMethod(){
 
-    public void buttonPressed(View view){
-        Log.i(LOG_TAG, "buttonPressed(View) called");
-
-        String stringValue = "Hello World!";
-
-        mTextView.setText(StringVlaue);
-
-        Log.v(LOG_TAG,"text change to:", stringValue);
-
-        log.d(LOG_TAG, "buttonPresssed(View) finished");
     }
 }
